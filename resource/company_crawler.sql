77476f580ba1308b605b712f9bca165b82a27ace
niujiaxi <niujiaxi@puhuifinance.com>
2016-03-01 19:07:50 +0800

    db

--- resource/company_crawler.sql
+++ resource/company_crawler.sql
@@ -1,1 +1,15 @@
-\
+create table company_crawler(
+  id int PRIMARY KEY     NOT NULL,
+  company_name VARCHAR(64),
+  product_name TEXT,
+  trade VARCHAR(64),
+  location VARCHAR(64),
+  stage TEXT,
+  management_team TEXT,
+  introduction TEXT,
+  company_url VARCHAR(64),
+  ext_info TEXT,
+  crawler_url VARCHAR(64),
+  create_time TimeStamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  crawler_Spider varchar(64)
+);
